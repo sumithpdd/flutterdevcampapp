37cb2a707143680837ff5368aa8660b678f67761
Joshua D. Mukiibi <joshuamukiibi10@gmail.com>
2022-09-21 22:17:07 +0300

    Separate Place initialization from data.dart

--- devcamp_session1/lib/models/data.dart
+++ devcamp_session1/lib/models/data.dart
@@ -21,15 +21,7 @@ List<Data> dataList = [
         "https://img.freepik.com/premium-photo/big-ben-clock-tower-thames-river-london_79295-6837.jpg?w=2000",
     cityInfo:
         "London is the capital and largest city of England and the United Kingdom, with a population of just over 9 million.",
-    cityPlaces: [
-      Place(
-        placeName: 'Buckingham Palace',
-        placeImage:
-            'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
-        placeDescription:
-            'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
-      ),
-    ],
+    cityPlaces: placesInLondon,
   ),
   Data(
     cityName: "Almaty",
@@ -37,29 +29,7 @@ List<Data> dataList = [
         "https://digitalnomads.world/wp-content/uploads/2021/11/almaty-digital-nomads-1024x683.jpg",
     cityInfo:
         "Almaty , orAlma-ata Алматы  is the largest city in Kazakhstan with a population of 1,421,868 (at 1st of August 2010) with about 9% of the countries population located in one city, it is certainly a hustling and bustling place to be. It also used to be the former capital of the country, until 1997 when it moved to Astana.",
-    cityPlaces: [
-      Place(
-        placeName: 'Shymbulak',
-        placeImage:
-            'https://astanatimes.com/wp-content/uploads/2020/01/20190314203151.jpg',
-        placeDescription:
-            'Shymbulak, also known as Chimbulak is a ski resort near Almaty. It is the largest ski resort in Central Asia. It is located in the upper part of the Medeu Valley in the Zailiisky Alatau mountain range, at an elevation of 2,200 metres above sea level.',
-      ),
-      Place(
-        placeName: 'Kök Töbe',
-        placeImage:
-            'https://img.itinari.com/pages/images/original/2e7cb0b4-9a72-4420-985b-166a406d7487-koktobe-6.jpg?ch=DPR&dpr=2.625&w=1600&s=1e0c611a5eb0eda7a46138daba045d67',
-        placeDescription:
-            'Kök Töbe is a mountain in Almaty, Kazakhstan\'s largest city. Kók Tóbe Park, a popular recreation area, is on top of the mountain. The mountain\'s height is 1100 meters above sea level',
-      ),
-      Place(
-        placeName: 'Medeu',
-        placeImage:
-            'https://aboutkazakhstan.com/blog/wp-content/uploads/2017/01/medeu-skating-rink-almaty-kazakhstan-7.jpg',
-        placeDescription:
-            'The Medeu, is an outdoor speed skating and bandy rink. It is located in a mountain valley on the south-eastern outskirts of Almaty, Kazakhstan',
-      ),
-    ],
+    cityPlaces: placesInAlmaty,
   ),
   Data(
     cityName: "Paris",
@@ -67,15 +37,7 @@ List<Data> dataList = [
         "https://img.freepik.com/premium-photo/seine-paris-with-eiffel-tower-autumn-season-paris-france_73503-527.jpg?w=2000",
     cityInfo:
         "Paris is the capital and most populous city of France, with an estimated population of 2,165,423 residents in 2019 in an area of more than 105 km².",
-    cityPlaces: [
-      Place(
-        placeName: 'Buckingham Palace',
-        placeImage:
-            'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
-        placeDescription:
-            'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
-      ),
-    ],
+    cityPlaces: placesInParis,
   ),
   Data(
     cityName: "NewYork",
@@ -83,15 +45,7 @@ List<Data> dataList = [
         "https://img.freepik.com/premium-photo/sunset-manhattan_110488-1459.jpg?w=2000",
     cityInfo:
         "New York, often called New York City (NYC) to distinguish it from the State of New York, is the most populous city in the United States.",
-    cityPlaces: [
-      Place(
-        placeName: 'Buckingham Palace',
-        placeImage:
-            'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
-        placeDescription:
-            'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
-      ),
-    ],
+    cityPlaces: placesInNewYork,
   ),
   Data(
     cityName: "Singapore",
@@ -99,15 +53,7 @@ List<Data> dataList = [
         "https://img.freepik.com/free-photo/singapore-cityscape-twilight_335224-686.jpg?w=2000&t=st=1663196035~exp=1663196635~hmac=81afdbf47d25cba65ede1566b0ad32794d60cce0930a6c422c96c1f62b75789d",
     cityInfo:
         "Singapore, city-state located at the southern tip of the Malay Peninsula, about 85 miles (137 kilometres) north of the Equator.",
-    cityPlaces: [
-      Place(
-        placeName: 'Buckingham Palace',
-        placeImage:
-            'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
-        placeDescription:
-            'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
-      ),
-    ],
+    cityPlaces: placesInSingapore,
   ),
   Data(
     cityName: "Prague",
@@ -115,15 +61,7 @@ List<Data> dataList = [
         "https://img.freepik.com/free-photo/prague-castle-from-vltava-side_1398-2758.jpg?w=1380&t=st=1663438901~exp=1663439501~hmac=46a6791051889dbe51ee239ae7011a7c7d105a84b06b2a9a79d7d368d18ce953",
     cityInfo:
         "Prague, is the capital and largest city in the Czech Republic, and the historical capital of Bohemia.",
-    cityPlaces: [
-      Place(
-        placeName: 'Buckingham Palace',
-        placeImage:
-            'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
-        placeDescription:
-            'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
-      ),
-    ],
+    cityPlaces: placesInPrague,
   ),
   Data(
     cityName: "Osaka",
@@ -131,15 +69,7 @@ List<Data> dataList = [
         "https://img.freepik.com/free-photo/cherry-blossoms-castle-osaka-japan_335224-169.jpg?w=1380&t=st=1663438937~exp=1663439537~hmac=8b576589b33fbaa82afb9a8e15efd49d0818d03a8425d5f27871a9380fd2647e",
     cityInfo:
         "Osaka is a large port city and commercial center on the Japanese island of Honshu.",
-    cityPlaces: [
-      Place(
-        placeName: 'Buckingham Palace',
-        placeImage:
-            'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
-        placeDescription:
-            'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
-      ),
-    ],
+    cityPlaces: placesInOsaka,
   ),
   Data(
     cityName: "Berlin",
@@ -147,14 +77,6 @@ List<Data> dataList = [
         "https://img.freepik.com/free-photo/building-with-columns_1160-803.jpg?w=1380&t=st=1663439085~exp=1663439685~hmac=db82c0f1a09750e9b47e63a0f271778f2ed36a8bc3fcfc589dbf9b24859ef2d8",
     cityInfo:
         "Berlin, Germany's capital, dates to the 13th century. Reminders of the city's turbulent 20th-century history include its Holocaust memorial and the Berlin Wall's graffitied remains.",
-    cityPlaces: [
-      Place(
-        placeName: 'Buckingham Palace',
-        placeImage:
-            'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
-        placeDescription:
-            'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
-      ),
-    ],
+    cityPlaces: placesInBerlin,
   ),
 ];
--- devcamp_session1/lib/models/place.dart
+++ devcamp_session1/lib/models/place.dart
@@ -10,4 +10,82 @@ class Place {
   });
 }
 
-// TODO (Joshua): Add list of places for each city here and import those into the dataList in Data.dart
+List<Place> placesInAlmaty = [
+  Place(
+    placeName: 'Shymbulak',
+    placeImage:
+        'https://astanatimes.com/wp-content/uploads/2020/01/20190314203151.jpg',
+    placeDescription:
+        'Shymbulak, also known as Chimbulak is a ski resort near Almaty. It is the largest ski resort in Central Asia. It is located in the upper part of the Medeu Valley in the Zailiisky Alatau mountain range, at an elevation of 2,200 metres above sea level.',
+  ),
+  Place(
+    placeName: 'Kök Töbe',
+    placeImage:
+        'https://img.itinari.com/pages/images/original/2e7cb0b4-9a72-4420-985b-166a406d7487-koktobe-6.jpg?ch=DPR&dpr=2.625&w=1600&s=1e0c611a5eb0eda7a46138daba045d67',
+    placeDescription:
+        'Kök Töbe is a mountain in Almaty, Kazakhstan\'s largest city. Kók Tóbe Park, a popular recreation area, is on top of the mountain. The mountain\'s height is 1100 meters above sea level',
+  ),
+  Place(
+    placeName: 'Medeu',
+    placeImage:
+        'https://aboutkazakhstan.com/blog/wp-content/uploads/2017/01/medeu-skating-rink-almaty-kazakhstan-7.jpg',
+    placeDescription:
+        'The Medeu, is an outdoor speed skating and bandy rink. It is located in a mountain valley on the south-eastern outskirts of Almaty, Kazakhstan',
+  ),
+];
+
+List<Place> placesInLondon = [
+  Place(
+    placeName: 'Buckingham Palace',
+    placeImage:
+        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
+    placeDescription:
+        'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
+  ),
+];
+List<Place> placesInSingapore = [
+  Place(
+    placeName: 'Buckingham Palace',
+    placeImage:
+        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
+    placeDescription:
+        'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
+  ),
+];
+List<Place> placesInParis = [];
+List<Place> placesInNewYork = [
+  Place(
+    placeName: 'Buckingham Palace',
+    placeImage:
+        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
+    placeDescription:
+        'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
+  ),
+];
+List<Place> placesInPrague = [
+  Place(
+    placeName: 'Buckingham Palace',
+    placeImage:
+        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
+    placeDescription:
+        'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
+  ),
+];
+List<Place> placesInOsaka = [
+  Place(
+    placeName: 'Buckingham Palace',
+    placeImage:
+        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
+    placeDescription:
+        'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
+  ),
+];
+List<Place> placesInBerlin = [
+  Place(
+    placeName: 'Buckingham Palace',
+    placeImage:
+        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
+    placeDescription:
+        'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
+  ),
+];
