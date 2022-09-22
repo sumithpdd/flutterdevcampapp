fa322b8401628c6154b0fe9a73d45ff1b0dcf9f1
Joshua D. Mukiibi <joshuamukiibi10@gmail.com>
2022-09-22 12:13:04 +0300

    Add a place or two for other cities

--- devcamp_session1/lib/models/place.dart
+++ devcamp_session1/lib/models/place.dart
@@ -80,49 +80,74 @@ List<Place> placesInLondon = [
     placeDescription: '',
   )
 ];
+
 List<Place> placesInSingapore = [
   Place(
-    placeName: 'Buckingham Palace',
+    placeName: 'Marina Bay Sands Singapore',
     placeImage:
-        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
+        'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcT6ji2E9X0OlCBb0_8D2aRqNE50ZtTLSHcPuQjIyBvAmKZE4ERQuUV5a7MYQjpbuRbf7Nh47T5EKANvCM5bBAZDPg',
     placeDescription:
-        'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
+        'Marina Bay Sands is an integrated resort fronting Marina Bay in Singapore. The resort is owned by the Las Vegas Sands Corporation and at its opening in 2010, it was deemed the world\'s most expensive standalone casino property at S\$8 billion.',
   ),
 ];
-List<Place> placesInParis = [];
+
+List<Place> placesInParis = [
+  Place(
+    placeName: 'Eiffel Tower',
+    placeImage:
+    'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQvnB0dD_MCmdoIGVGsC5tJdMxu5hjKag-mVuc8ZcbcBsJO0Yr-EtxTIU5Md9h3qoFaynWEh-qz4iuOwAbP_f7_7A',
+    placeDescription:
+    'The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower. Locally nicknamed "La dame de fer", it was constructed from 1887 to 1889 as the centerpiece of the 1889 World\'s Fair.',
+  ),
+  Place(
+    placeName: 'Louvre Museum',
+    placeImage:
+    'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcTqcSb8tBeNaPKGEWGQGTabDdxhSNbQDO_M6AcD-qoe7Or7tLCpKeM1Dw4yC5sa_BHCm8jIuEIQy4Wfd2vF0EXamg',
+    placeDescription:
+    'The Louvre, or the Louvre Museum, is the world\'s most-visited museum, and a historic landmark in Paris, France. It is the home of some of the best-known works of art, including the Mona Lisa and the Venus de Milo. A central landmark of the city, it is located on the Right Bank of the Seine in the city\'s 1st arrondissement.',
+  ),
+  Place(
+    placeName: 'Arc de Triomphe',
+    placeImage:
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQoampQRxxyGjB5ERXi9nTc3YCFd6F8XyNX_w2peRJmn85owFVdD1tiQ3Ny4vGDXF4VnxNAzQi5FCDhn1P-P8DAQ',
+    placeDescription:
+    'The Arc de Triomphe de l\'Étoile is one of the most famous monuments in Paris, France, standing at the western end of the Champs-Élysées at the centre of Place Charles de Gaulle, formerly named Place de l\'Étoile—the étoile or "star" of the juncture formed by its twelve radiating avenues. The location of the arc and the plaza is shared between three arrondissements, 16th, 17th, and 8th. The Arc de Triomphe honours those who fought and died for France in the French Revolutionary and Napoleonic Wars, with the names of all French victories and generals inscribed on its inner and outer surfaces.',
+  ),
+];
+
 List<Place> placesInNewYork = [
   Place(
-    placeName: 'Buckingham Palace',
+    placeName: 'Central Park',
     placeImage:
-        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUhfITNdazc_b-fvsQViOkn4alkFX_N7H8dp6TAaDZdWnpSeVr4QO17B1smrXWRJ4QE7Mrixpp9gVxIpMkqQQxIA',
     placeDescription:
-        'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
+        'Central Park is an urban park in New York City, between the Upper West and Upper East Sides of Manhattan. It is the fifth-largest park in the city, covering 843 acres. It is the most visited urban park in the United States, with an estimated 42 million visitors annually as of 2016, and is the most filmed location in the world.',
   ),
 ];
 List<Place> placesInPrague = [
   Place(
-    placeName: 'Buckingham Palace',
+    placeName: 'Prague Castle',
     placeImage:
-        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjEEwv-xRNi0TalOol10W0TjN3ePSNt391Qmh3NW7SRME41QzrT7gLLOis9hlvqa6iPYxQhL-jAjVej1AQTqW86A',
     placeDescription:
-        'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
+        'Prague Castle is a castle complex in Prague, Czech Republic, built in the 9th century. It is the official office of the President of the Czech Republic. The castle was a seat of power for kings of Bohemia, Holy Roman emperors, and presidents of Czechoslovakia.',
   ),
 ];
 List<Place> placesInOsaka = [
   Place(
-    placeName: 'Buckingham Palace',
+    placeName: 'Osaka Castle',
     placeImage:
-        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
+        'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTFOCZIk-Yerr-53N8JxizWjAwCTetEKJsLSF3kAfhz7Hpibej8yz9XZexOw__-QZvN6l9ItYGIhKtySwcpZ9Id8Q',
     placeDescription:
-        'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
+        'Osaka Castle is a Japanese castle in Chūō-ku, Osaka, Japan. The castle is one of Japan\'s most famous landmarks and it played a major role in the unification of Japan during the sixteenth century of the Azuchi-Momoyama period.',
   ),
 ];
 List<Place> placesInBerlin = [
   Place(
-    placeName: 'Buckingham Palace',
+    placeName: 'Brandenburg Gate',
     placeImage:
-        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRpDk9T8pPp5Vh-Y7fOPmF7DPqv9PNHntbdgnBJPfTzPzWO2VnoSBbQs3NtLEjOrp4NtwrVtS6yujkCjNKVDOzgOA',
+        'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSO04xSWIqYc6dgJmRsXyw8E9GYY-QgRe0JnRUeEEdk_5iHkLa47kMqiLPG_t0rSPLBHCuvA0rD03MP41f7AsZZbw',
     placeDescription:
-        'Buckingham Palace is the London residence and administrative headquarters of the monarchy of the United Kingdom.',
+        'The Brandenburg Gate is an 18th-century neoclassical monument in Berlin, built on the orders of Prussian king Frederick William II after restoring the Orangist power by suppressing the Dutch popular unrest. One of the best-known landmarks of Germany, it was built on the site of a former city gate that marked the start of the road from Berlin to the town of Brandenburg an der Havel, which used to be the capital of the Margraviate of Brandenburg.',
   ),
 ];
