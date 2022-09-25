1e549fb2f34c8f874c0c44bf1b01c2ac041779a7
Joshua D. Mukiibi <joshuamukiibi10@gmail.com>
2022-09-25 17:52:07 +0300

    Add custom bulleted_list

---
+++ recipe_app_ui/lib/custom_widgets/bullet_list_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class BulletList extends StatelessWidget {
+  final List<String> strings;
+
+  const BulletList({
+    super.key,
+    required this.strings,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: const EdgeInsets.fromLTRB(16, 15, 16, 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: strings.map((str) {
+          return Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                '\u2022',
+                style: TextStyle(
+                  fontSize: 16,
+                  height: 1.55,
+                ),
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Expanded(
+                child: Text(
+                  str,
+                  textAlign: TextAlign.left,
+                  softWrap: true,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black.withOpacity(0.6),
+                    height: 1.55,
+                  ),
+                ),
+              ),
+            ],
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
--- recipe_app_ui/lib/custom_widgets/custom_widgets.dart
+++ recipe_app_ui/lib/custom_widgets/custom_widgets.dart
@@ -1,3 +1,5 @@
 export 'search_box.dart';
 export 'recipe_card.dart';
 export 'filter_list_view.dart';
+export 'ingredients_and_directions.dart';
+export 'bullet_list_widget.dart';
