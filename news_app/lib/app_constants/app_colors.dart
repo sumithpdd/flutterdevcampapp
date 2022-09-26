52ab1b991bcf3f3e5fa2098d893a0b4ee37a99d6
Joshua D. Mukiibi <joshuamukiibi10@gmail.com>
2022-09-26 18:35:06 +0300

    Add AppColors

--- news_app/lib/app_constants/app_colors.dart
+++ news_app/lib/app_constants/app_colors.dart
@@ -1,3 +1,36 @@
+import 'package:flutter/material.dart';
+
 class AppColors {
   // TODO (Joshua): Implement AppColors
+  // static const Color primary;
+  // static const Color secondary;
+  static const Color black = Color(0xff0a0a0a);
+  static const Color white = Color(0xffffffff);
+
+  static Color getShade(Color color, {bool darker = false, double value = .1}) {
+    assert(value >= 0 && value <= 1);
+
+    final hsl = HSLColor.fromColor(color);
+    final hslDark = hsl.withLightness(
+        (darker ? (hsl.lightness - value) : (hsl.lightness + value))
+            .clamp(0.0, 1.0));
+
+    return hslDark.toColor();
+  }
+
+  static MaterialColor getMaterialColorFromColor(Color color) {
+    Map<int, Color> colorShades = {
+      50: getShade(color, value: 0.5),
+      100: getShade(color, value: 0.4),
+      200: getShade(color, value: 0.3),
+      300: getShade(color, value: 0.2),
+      400: getShade(color, value: 0.1),
+      500: color, //Primary value
+      600: getShade(color, value: 0.1, darker: true),
+      700: getShade(color, value: 0.15, darker: true),
+      800: getShade(color, value: 0.2, darker: true),
+      900: getShade(color, value: 0.25, darker: true),
+    };
+    return MaterialColor(color.value, colorShades);
+  }
 }
